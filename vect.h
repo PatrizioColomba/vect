3c63d86264292d2b5e922b62b4299fef2eae35db
Patrizio Colomba <patrizio.colomba@gmail.com>
2025-02-23 11:29:22 +0000

    feat: enhance vector functionality with add and get operations, improve output formatting

--- vect.h
+++ vect.h
@@ -3,12 +3,21 @@
 #ifndef __STRVECT_H__
 #define __STRVECT_H__
 
+#define PRINT_VECT_INT(vector) \
+    printf("vector (size %zu)\n", vector->size); \
+    for (size_t i = 0; i < vector->size; i++) { \
+        printf("%zu = %d\n", i, *(int *)vector->data[i]); \
+    }
+
 typedef struct vect {
     void **data;
     size_t size;
 } Vector;
+
 Vector *init_vect(size_t n, ...);
 void free_vect(Vector *vector, const void (*free_item)(void *));
 Vector *append_vect(Vector *vector1, Vector *vector2);
+Vector *add_vect(Vector *vector, void *element);
+void *get_vect(Vector *vector, size_t index);
 
 #endif
