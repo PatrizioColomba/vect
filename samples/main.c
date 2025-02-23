2889dbf9661e5d1b4fa81cd2dd770ae2ec39a5a4
Patrizio Colomba <patrizio.colomba@gmail.com>
2025-02-23 09:47:12 +0000

    style: improve code formatting and consistency in main.c and vect.c

--- samples/main.c
+++ samples/main.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include "../vect.h"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     size_t size;
     printf("Enter the vector size: ");
     scanf("%zu", &size);
     int n1 = 4;
     int n2 = 13;
-    Vector *vector = init_vect(size, (void*)&n1, (void*)&n2);
+    Vector *vector = init_vect(size, (void *)&n1, (void *)&n2);
 
     if (vector == NULL) {
         printf("vector is null!\n");
@@ -17,7 +18,7 @@ int main(int argc, char** argv) {
 
     printf("First vector (size %zu)\n", vector->size);
     for (int i = 0; i < vector->size; i++) {
-        printf("%d = %d\n", i, *(int*)vector->data[i]);
+        printf("%d = %d\n", i, *(int *)vector->data[i]);
     }
 
     Vector *new_vector = append_vect(vector, vector);
@@ -25,7 +26,7 @@ int main(int argc, char** argv) {
 
     printf("Second vector (size %zu)\n", new_vector->size);
     for (int i = 0; i < new_vector->size; i++) {
-        printf("%d = %d\n", i, *(int*)new_vector->data[i]);
+        printf("%d = %d\n", i, *(int *)new_vector->data[i]);
     }
 
     free_vect(new_vector, NULL);
