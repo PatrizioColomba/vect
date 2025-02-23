ac28140549a438224e658c0ee68fd69ed670cfab
Patrizio Colomba <patrizio.colomba@gmail.com>
2025-02-23 09:47:12 +0000

    style: improve code formatting and consistency in main.c and vect.c

--- vect.c
+++ vect.c
@@ -4,32 +4,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-Vector *init_vect(size_t n, ...) {
-  if (n < 0) {
-    printf("n must be grater than zero!\n");
-    return NULL;
-  }
+Vector *init_vect(size_t n, ...)
+{
+    if (n < 0) {
+        printf("n must be grater than zero!\n");
+        return NULL;
+    }
 
-  void **data = malloc(n * sizeof(void **));
-  va_list args;
-  va_start(args, n);
+    void **data = malloc(n * sizeof(void **));
+    va_list args;
+    va_start(args, n);
 
-  void **p = data;
-  for (int i = 0; i < n; i++) {
-    *p = va_arg(args, void *);
-    p++;
-  }
+    void **p = data;
+    for (int i = 0; i < n; i++) {
+        *p = va_arg(args, void *);
+        p++;
+    }
 
-  va_end(args);
+    va_end(args);
 
-  Vector *vect = malloc(sizeof(Vector));
-  vect->data = data;
-  vect->size = n;
-  
-  return vect;
+    Vector *vect = malloc(sizeof(Vector));
+    vect->data = data;
+    vect->size = n;
+
+    return vect;
 }
 
-void free_vect(Vector *vector, const void (*free_item)(void *)) {
+void free_vect(Vector *vector, const void (*free_item)(void *))
+{
     if (vector == NULL) {
         printf("parameter vector is null!\n");
         return;
@@ -42,11 +44,12 @@ void free_vect(Vector *vector, const void (*free_item)(void *)) {
         }
     }
 
-    free(vector->data); // Libera i dati del vettore
-    free(vector); // Libera la struttura del vettore
+    free(vector->data);  // Libera i dati del vettore
+    free(vector);        // Libera la struttura del vettore
 }
 
-Vector *append_vect(Vector *vector1, Vector *vector2) {
+Vector *append_vect(Vector *vector1, Vector *vector2)
+{
     if (vector1 == NULL) {
         printf("parameter vector1 is null!\n");
         return NULL;
